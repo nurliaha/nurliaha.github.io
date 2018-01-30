916440776d154c5ebda5ef1d9d2de08f5791c727
nurliaha <33761621+nurliaha@users.noreply.github.com>
2018-01-30 20:47:02 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+
+<?php
+
+/* membuat file config.php */
+
+	$databaseHost = 'localhost';
+	$databaseName = 'crud_db';
+	$databaseUserName = 'root';
+	$databasePassword = '';
+
+	$mysqli = mysqli_connect($databaseHost, $databaseUserName, $databasePassword, $databaseName);
+?>
+
